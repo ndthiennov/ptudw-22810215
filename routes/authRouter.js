357d1184cdb0711fc2b851b6df21659806fd01db
ndthiennov <ndthiennov@gmail.com>
2025-05-21 21:01:26 +0700

    validator for login form

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,8 +3,19 @@
 let express = require('express');
 let router = express.Router();
 let controller = require('../controllers/authController');
+const {body, getErrorMessage} = require('../controllers/validator');
+
 
 router.get('/login', controller.show);
-router.post('/login', controller.login);
+router.post('/login', 
+    body('email').trim().notEmpty().withMessage('Email is required!').isEmail().withMessage('Invalid email address'),
+    body('password').trim().notEmpty().withMessage('Password is required'),
+    (req,res,next)=>{
+        let message = getErrorMessage(req);
+        if(message){
+            return res.render('login', {loginMessage:message});
+        }
+        next();
+    },controller.login);
 
 module.exports = router;
