1b657277035079a0feef69000aa773101be0da62
ndthiennov <ndthiennov@gmail.com>
2025-05-21 21:11:41 +0700

    fix cart after and before login logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,6 +9,7 @@ controller.show = (req, res) => {
 
 controller.login = (req, res, next) => {
     let keepSignedIn = req.body.keepSignedIn;
+    let cart = req.session.cart;
     passport.authenticate('local-login', (error, user) => {
         if (error) {
             return next(error);
@@ -19,14 +20,17 @@ controller.login = (req, res, next) => {
         req.logIn(user, (error) => {
             if (error) { return next(error); }
             req.session.cookie.maxAge = keepSignedIn ? (24 * 60 * 60 * 1000) : null;
+            req.session.cart = cart;
             return res.redirect('/users/my-account');
         });
     })(req, res, next);
 }
 
 controller.logout = (req, res, next) => {
+    let cart = req.session.cart;
     req.logout((error) => {
         if (error) { return next(error); }
+        req.session.cart = cart;
         res.redirect('/');
     })
 }
