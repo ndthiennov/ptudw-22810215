c9164a2d3f118c98f9cea7525be51c7cd46a34dc
ndthiennov <ndthiennov@gmail.com>
2025-05-21 21:31:04 +0700

    security for accessing required authentication page

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,24 +4,29 @@ const controller = {}
 const passport = require('./passport')
 
 controller.show = (req, res) => {
-    res.render('login', { loginMessage: req.flash('loginMessage') });
+    if (req.isAuthenticated()) {
+        return res.redirect('/');
+    }
+    res.render('login', { loginMessage: req.flash('loginMessage'), reqUrl: req.query.reqUrl });
 }
 
 controller.login = (req, res, next) => {
     let keepSignedIn = req.body.keepSignedIn;
+    let reqUrl = req.body.reqUrl ? req.body.reqUrl : '/users/my-account';
     let cart = req.session.cart;
     passport.authenticate('local-login', (error, user) => {
         if (error) {
             return next(error);
         }
         if (!user) {
-            return res.redirect('/users/login');
+            return res.redirect(`/users/login?reqUrl=${reqUrl}`);
         }
         req.logIn(user, (error) => {
             if (error) { return next(error); }
             req.session.cookie.maxAge = keepSignedIn ? (24 * 60 * 60 * 1000) : null;
             req.session.cart = cart;
-            return res.redirect('/users/my-account');
+            //return res.redirect('/users/my-account');
+            return res.redirect(reqUrl);
         });
     })(req, res, next);
 }
@@ -33,6 +38,13 @@ controller.logout = (req, res, next) => {
         req.session.cart = cart;
         res.redirect('/');
     })
+}
+
+controller.isLoggedIn = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect(`/users/login?reqUrl=${req.originalUrl}`);
 }
 
 module.exports = controller;
